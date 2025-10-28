39f54b0b37dcd963c49c5c0426273abb74d22716
DanishAqib <danishaqib129@gmail.com>
2025-10-28 17:07:50 +0500

    changed tab title

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function HomePage() {
   return (
     <>
       <Head>
-        <title>FIXHERO</title>
-        <meta name="description" content="Fixhero info" />
+        <title>137NL</title>
+        <meta name="description" content="137NL info" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
